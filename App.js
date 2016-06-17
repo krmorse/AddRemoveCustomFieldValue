d89f6e31fd0edb2fadcc016dce60e88bfc6ffa63
Kyle Morse <kmorse@rallydev.com>
2016-06-17 15:36:25 -0600

    Add field creation to example

--- App.js
+++ App.js
@@ -1,25 +1,52 @@
 Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
+
     launch: function() {
         Rally.data.wsapi.ModelFactory.getModels({
-          types: ['userstory', 'allowedattributevalue']
+          types: ['userstory', 'attributedefinition', 'allowedattributevalue']
         }).then({
-          success: function(models) {
-            var field = models.userstory.getField('c_KanbanState');
-            var newValue = Ext.create(models.allowedattributevalue, {
-              StringValue: 'D',
-              ValueIndex: 0,
-              AttributeDefinition: Rally.util.Ref.getRelativeUri(field.attributeDefinition)
-            });
-            newValue.save().then({
-              success: function() {
-                console.log(arguments);
-              },
-              scope: this
-            });
-          },
+          success: this._createField,
           scope: this
         });
+    },
+
+    _createField: function(models) {
+        this.models = models;
+
+        //hack to make required fields persistable
+        _.each(['AttributeType', 'RealAttributeType', 'Constrained', 'TypeDefinition'], function(fieldName) {
+          this.models.attributedefinition.getField(fieldName).persist = true;
+        }, this);
+        //end hack
+
+        var newField = Ext.create(this.models.attributedefinition, {
+          AttributeType: 'STRING',
+          RealAttributeType: 'DROP_DOWN',
+          Constrained: true,
+          Custom: true,
+          Filterable: true,
+          Sortable: true,
+          Name: 'MyDropdownField',
+          TypeDefinition: this.models.userstory.typeDefinition._ref
+        });
+        newField.save().then({
+          success: this._createValue,
+          scope: this
+        });
+    },
+
+    _createValue: function(field) {
+      var newValue = Ext.create(this.models.allowedattributevalue, {
+        StringValue: 'D',
+        ValueIndex: 0,
+        AttributeDefinition: Rally.util.Ref.getRelativeUri(field)
+      });
+      newValue.save().then({
+        success: function() {
+          console.log('winning');
+        },
+        scope: this
+      });
     }
 });
--- README.md
+++ README.md
@@ -1,13 +1,5 @@
 AddRemoveCustomFieldValue
 =========================
 
-## Overview
-
-
-## License
-
-AddRemoveCustomFieldValue is released under the MIT license.  See the file [LICENSE](./LICENSE) for the full text.
-
-##Documentation for SDK
-
-You can find the documentation on our help [site.](https://help.rallydev.com/apps/2.1/doc/)
+This app is an example of how to create a custom dropdown field
+on a user story and add an allowed value to it.
